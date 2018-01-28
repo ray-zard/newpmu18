49fd195fec208ad8e2f83bb035dd7543f2202dd5
ray-zard <ray-zard@mail.ru>
2018-01-28 18:32:06 +0400

    Первый коммит, + календарик

---
+++ src/assets/js/app.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+
+window.$ = $;
+
+import Popper from 'popper.js';
+import Bootstrap from 'bootstrap';
+import AirDatePicker from 'air-datepicker';
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+// import './lib/foundation-explicit-pieces';
+
+
+$(document).ready(function() {
+
+	$('#sandbox-container').datepicker({
+		language: 'my-lang'
+	});
+
+});
