cd8b50bc27c3fb3cac0a1a72ff9fb5a6d5bb752d
ray-zard <ray-zard@mail.ru>
2018-02-02 01:48:22 +0400

    Добил карты новостей

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 window.$ = $;
 
 import Popper from 'popper.js';
+import BaguetteBox from 'baguettebox.js';
 import Bootstrap from 'bootstrap';
 import AirDatePicker from 'air-datepicker';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
@@ -15,5 +16,7 @@ $(document).ready(function() {
 	$('#sandbox-container').datepicker({
 		language: 'my-lang'
 	});
+
+	BaguetteBox.run('.gallery');
 
 });
